19ddb345e5f99c2ace7dacd98d7f0c2f079156bc
LTIEU1 <LTIEU1@ford.com>
2020-08-23 14:42:19 +0700

    CriminalIntent using ConstrainLayout

--- CriminalIntent/app/src/main/java/com/tieuthanhliem/android/criminalintent/CrimeListFragment.kt
+++ CriminalIntent/app/src/main/java/com/tieuthanhliem/android/criminalintent/CrimeListFragment.kt
@@ -1,12 +1,15 @@
 package com.tieuthanhliem.android.criminalintent
 
 import android.os.Bundle
+import android.text.format.DateFormat
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -54,6 +57,7 @@ class CrimeListFragment : Fragment() {
         private lateinit var crime: Crime
         private var titleTextView: TextView = itemView.findViewById(R.id.crime_title)
         private var dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+        private var solvedImageView: ImageView = itemView.findViewById(R.id.crime_solved)
 
         init {
             itemView.setOnClickListener(this)
@@ -62,7 +66,12 @@ class CrimeListFragment : Fragment() {
         fun bind(crime: Crime) {
             this.crime = crime
             titleTextView.text = this.crime.title
-            dateTextView.text  = this.crime.date.toString()
+            dateTextView.text  = DateFormat.format("EEEE, MMM d, yyyy", this.crime.date)
+            solvedImageView.visibility = if(crime.isSolved) {
+                View.VISIBLE
+            } else {
+                View.GONE
+            }
         }
 
         override fun onClick(v: View?) {
@@ -88,6 +97,7 @@ class CrimeListFragment : Fragment() {
         }
 
         override fun getItemViewType(position: Int): Int {
+            return VIEW_TYPE_NORMAL_CRIME
             return when {
                 crimes[position].requiresPolice -> VIEW_TYPE_SERIOUS_CRIME
                 else -> VIEW_TYPE_NORMAL_CRIME
